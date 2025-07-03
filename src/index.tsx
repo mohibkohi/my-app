f0dcc9d64ba76af1bb59feb3a7284d7776631a08
mohibkohi <mohibkohi@yahoo.com>
2025-07-02 21:18:18 -0700

    added dark background

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './CSS/index.css'; // Ensure this is the correct path to your CSS file
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
